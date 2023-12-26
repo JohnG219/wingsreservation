37928e85381a5bb87488826e587d99964149fee9
JohnE2199 <138347578+JohnG219@users.noreply.github.com>
2023-12-26 10:50:15 +0800

    Update auth.js

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -25,8 +25,8 @@ export const register = async (req, res, next) => {
 export const login = async (req, res, next) => {
   try {
     
-    const user = await User.findOne({ username: req.body.username });
-    if (!user) return next(createError(404, "Invalid Username"));
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return next(createError(404, "Invalid Credentials"));
     const isPasswordCorrect = await bcrypt.compare(
       req.body.password,
       user.password
